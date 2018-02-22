89593a9769c19a8f46be795f01341844f52ba3f9
Felix Tjandrawibawa <felix.tjandrawibawa@fairfaxmedia.com.au>
2018-02-22 13:07:32 +1100

    Following along gowebexample

--- src/github.com/cemeng/golang-playground/hello/hello_world.go
+++ src/github.com/cemeng/golang-playground/hello/hello_world.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"log"
+	"net/http"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome this is the root")
+}
+
+func sayHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	nameFromPost := r.FormValue("name")
+	fmt.Fprintf(w, "Say my name, get name: %s, post name: %s", name, nameFromPost)
+}
+
 func main() {
-  fmt.Println("Hello world")
+	http.HandleFunc("/", rootHandler)
+
+	// how do I indicate if a route should be accessible by POST or GET only?
+	// by adding Methods call at the end
+	http.HandleFunc("/say", sayHandler)
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// r.URL.Path
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
