220b46eeeaa58976956b1b147c6ad8f0949ea4da
Felix Tjandrawibawa <felix.tjandrawibawa@fairfaxmedia.com.au>
2018-02-22 13:39:06 +1100

    More fun wiht mux

--- src/github.com/cemeng/golang-playground/hello/hello_world.go
+++ src/github.com/cemeng/golang-playground/hello/hello_world.go
@@ -11,28 +11,29 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome this is the root")
 }
 
-func sayHandler(w http.ResponseWriter, r *http.Request) {
+func showNameHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	nameFromPost := r.FormValue("name")
+	vars := mux.Vars(r)
+	nameFromMux := vars["name"]
+
+	fmt.Fprintf(w, "Say my name, get name: %s, post name: %s, mux name: %s", name, nameFromPost, nameFromMux)
+}
+
+func createNameHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
 	nameFromPost := r.FormValue("name")
 	fmt.Fprintf(w, "Say my name, get name: %s, post name: %s", name, nameFromPost)
 }
 
-func booksHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	title := vars["title"]
-	page := vars["page"]
-
-	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-}
-
 func main() {
 	r := mux.NewRouter()
 	fs := http.FileServer(http.Dir("static/"))
-
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/say", sayHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	r.HandleFunc("/books/{title}/page/{page}", booksHandler)
+	r.HandleFunc("/", rootHandler)
+	r.HandleFunc("/names/{name}", showNameHandler).Methods("GET")
+	r.HandleFunc("/names/", createNameHandler).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
